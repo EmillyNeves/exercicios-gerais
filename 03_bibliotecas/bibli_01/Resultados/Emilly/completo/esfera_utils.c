53503aa688ee114bbad2694acce941c2e2b4cdf3
EmillyNeves <emilly.neves@edu.ufes.br>
2024-04-03 15:41:17 -0300

    add bibli

---
+++ 03_bibliotecas/bibli_01/Resultados/Emilly/completo/esfera_utils.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include "esfera_utils.h"
+#include "math.h"
+
+float calcula_volume (float R){
+    return PI*pow(R,3)*4/3;
+}
+
+float calcula_area (float R){
+    return 4*PI*pow(R,2);
+}
